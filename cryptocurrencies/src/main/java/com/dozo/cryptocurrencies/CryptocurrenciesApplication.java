735b536233e0721befb95e7ec755cc4ac940f48c
Tomás Dozo <tomasdozo@hotmail.com>
2023-04-13 21:09:37 -0300

    initial commit

---
+++ cryptocurrencies/src/main/java/com/dozo/cryptocurrencies/CryptocurrenciesApplication.java
@@ -0,0 +1,13 @@
+package com.dozo.cryptocurrencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocurrenciesApplication.class, args);
+	}
+
+}
---
+++ my-microservice/src/main/java/com/dozo/mymicroservice/MyMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dozo.mymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMicroserviceApplication.class, args);
+	}
+
+}
